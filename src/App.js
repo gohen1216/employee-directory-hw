25c6659117d3e8440862702a15511368e6a87c62
Sanusi Cole <sanusi1612@icloud.com>
2021-09-23 17:38:32 -0400

    prints user roads

--- src/App.js
+++ src/App.js
@@ -19,16 +19,12 @@ function App() {
     <th>Contact</th>
     <th>Country</th>
   </tr>
-  <tr>
+  
+  {users.map(value=>{return <tr>
     <td>Alfreds Futterkiste</td>
     <td>Maria Anders</td>
     <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
+  </tr>})}
 </Table>
     </div>
   );
